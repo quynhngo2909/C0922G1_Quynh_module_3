d140651b000e90e70ce2388e26030bbb093329f0
quynhngo2909 <dieuquynh105@gmail.com>
2022-12-14 15:23:33 +0700

    nop bt

---
+++ ss1_rdb/ss1_exercise.sql
@@ -0,0 +1,18 @@
+create schema if not exists `student_management`;
+USE `student_management`;
+create table `student`(
+`id` int not null,
+`name` varchar(45) null,
+`age` int null,
+`country` varchar(45),
+primary key(`id`) );
+
+create table `class` (
+`id` int not null primary key,
+`name` varchar(45) null);
+
+create table `teacher` (
+`id` int not null primary key,
+`name` varchar(45) null,
+`age` int null,
+`country` varchar(45) null);
