74ac17d1a3ecf384ad29c72554c8f8b8124c3692
quynhngo2909 <dieuquynh105@gmail.com>
2022-12-14 15:25:38 +0700

    nop bt

--- ss1_rdb/ss1_exercise.sql
+++ ss1_rdb/ss1_exercise.sql
@@ -1,18 +1,21 @@
 create schema if not exists `student_management`;
-USE `student_management`;
+use `student_management`;
 create table `student`(
-`id` int not null,
-`name` varchar(45) null,
-`age` int null,
-`country` varchar(45),
-primary key(`id`) );
+  `id` int not null, 
+  `name` varchar(45) null, 
+  `age` int null, 
+  `country` varchar(45), 
+  primary key(`id`)
+);
 
 create table `class` (
-`id` int not null primary key,
-`name` varchar(45) null);
+  `id` int not null primary key, 
+  `name` varchar(45) null
+);
 
 create table `teacher` (
-`id` int not null primary key,
-`name` varchar(45) null,
-`age` int null,
-`country` varchar(45) null);
+  `id` int not null primary key, 
+  `name` varchar(45) null, 
+  `age` int null, 
+  `country` varchar(45) null
+);
--- ss1_rdb/ss1_practice.sql
+++ ss1_rdb/ss1_practice.sql
@@ -1,1 +1,8 @@
-create database student_management;
+create database if not exists student_management;
+create table `student_management`.`student_table`(
+  `id` int not null, 
+  `name` varchar(45) null, 
+  `age` int null, 
+  `country` varchar(45), 
+  primary key(`id`)
+);
