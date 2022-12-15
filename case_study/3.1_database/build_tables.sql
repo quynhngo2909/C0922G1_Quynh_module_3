8f22f77673cd495f463a020d5d73c8629e51cd7d
quynhngo2909 <dieuquynh105@gmail.com>
2022-12-15 21:23:15 +0700

    nop bt

--- case_study/3.1_database/build_tables.sql
+++ case_study/3.1_database/build_tables.sql
@@ -4,14 +4,17 @@ create table vi_tri (
   ma_vi_tri int primary key, 
   ten_vi_tri varchar(45)
 );
+
 create table trinh_do (
   ma_trinh_do int primary key, 
   ten_trinh_do varchar(45)
 );
+
 create table bo_phan (
   ma_bo_phan int primary key, 
   ten_bo_phan varchar(45)
 );
+
 create table nhan_vien (
   ma_nhan_vien int primary key, 
   ho_ten varchar(45) not null, 
@@ -28,10 +31,12 @@ create table nhan_vien (
   foreign key (ma_trinh_do) references trinh_do(ma_trinh_do), 
   foreign key (ma_bo_phan) references bo_phan(ma_bo_phan)
 );
+
 create table loai_khach (
   ma_loai_khach int primary key, 
   ten_loai_khach varchar(45)
 );
+
 create table khach_hang (
   ma_khach_hang int primary key, 
   ma_loai_khach int, 
@@ -44,14 +49,17 @@ create table khach_hang (
   dia_chi varchar(45), 
   foreign key (ma_loai_khach) references loai_khach(ma_loai_khach)
 );
+
 create table kieu_thue (
   ma_kieu_thue int primary key, 
   ten_kieu_thue varchar(45)
 );
+
 create table loai_dich_vu (
   ma_loai_dich_vu int primary key, 
   ten_loai_dich_vu varchar(45)
 );
+
 create table dich_vu (
   ma_dich_vu int primary key, 
   ten_dich_vu varchar(45) not null, 
@@ -68,6 +76,7 @@ create table dich_vu (
   foreign key (ma_kieu_thue) references kieu_thue(ma_kieu_thue), 
   foreign key (ma_loai_dich_vu) references loai_dich_vu(ma_loai_dich_vu)
 );
+
 create table hop_dong (
   ma_hop_dong int primary key, 
   ngay_lam_hop_dong datetime, 
@@ -80,6 +89,7 @@ create table hop_dong (
   foreign key (ma_khach_hang) references khach_hang(ma_khach_hang), 
   foreign key (ma_dich_vu) references dich_vu(ma_dich_vu)
 );
+
 create table dich_vu_di_kem (
   ma_dich_vu_di_kem int primary key, 
   ten_dich_vu_di_kem varchar(45) not null, 
@@ -87,6 +97,7 @@ create table dich_vu_di_kem (
   don_vi varchar(10) not null, 
   trang_thai varchar(45)
 );
+
 create table hop_dong_chi_tiet (
   ma_hop_dong_chi_tiet int primary key, 
   ma_hop_dong int, 
