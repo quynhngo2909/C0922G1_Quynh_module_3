348a303eac058ab5c764a2e6e72202a09b6bb05f
quynhngo2909 <dieuquynh105@gmail.com>
2022-12-16 17:06:21 +0700

    nop bt

--- ss3_database_query/ss3_exercise/database_query_with_student_management_db.sql
+++ ss3_database_query/ss3_exercise/database_query_with_student_management_db.sql
@@ -1,42 +1,12 @@
 use quan_ly_sinh_vien;
-select 
-  * 
-from 
-  student 
-where 
-  student_name like 'h%';
+select * from student where student_name like 'h%';
+select * from class where extract(month from start_date) = '12';
+select * from subject where credit between 3 and 5;
   
-select 
-  * 
-from 
-  class 
-where 
-  extract(
-    month 
-    from 
-      start_date
-  ) = '12';
+set sql_safe_updates = 0;
+update student set class_ID = 2 where student_name = 'Hung';
   
-select 
-  * 
-from 
-  subject 
-where 
-  credit between 3 
-  and 5;
-  
-set 
-  sql_safe_updates = 0;
-update 
-  student 
-set 
-  class_ID = 2 
-where 
-  student_name = 'Hung';
-  
-set 
-  sql_safe_updates = 1;
-  
+set sql_safe_updates = 1;
 select 
   S.student_name, 
   Sub.sub_name, 
