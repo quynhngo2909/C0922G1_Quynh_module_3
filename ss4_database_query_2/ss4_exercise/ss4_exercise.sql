57cae1d0cb2423b0f7a9407ea69453a83c1988f4
quynhngo2909 <dieuquynh105@gmail.com>
2022-12-19 11:36:07 +0700

    nop bt

--- ss4_database_query_2/ss4_exercise/ss4_exercise.sql
+++ ss4_database_query_2/ss4_exercise/ss4_exercise.sql
@@ -5,9 +5,9 @@ select *
     where s.credit =(select max(s.credit) from subject s);
 
 -- Hiển thị các thông tin môn học có điểm thi lớn nhất.
-	select s.sub_name, m.mark 
-		from mark m join subject s on m.sub_id = s.sub_id
-		where m.mark = (select max(m.mark) from mark m);
+select s.sub_name, m.mark 
+	from mark m join subject s on m.sub_id = s.sub_id
+	where m.mark = (select max(m.mark) from mark m);
 			
 -- Hiển thị các thông tin sinh viên và điểm trung bình của mỗi sinh viên, xếp hạng theo thứ tự điểm giảm dần
 set sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
