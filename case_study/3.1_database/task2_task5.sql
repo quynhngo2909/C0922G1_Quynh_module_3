69d9255cf8de844637a7036d0545fb9bd5df7e2f
quynhngo2909 <dieuquynh105@gmail.com>
2022-12-21 08:20:35 +0700

    nop bt

--- case_study/3.1_database/task2_task5.sql
+++ case_study/3.1_database/task2_task5.sql
@@ -4,12 +4,9 @@ use furama_database;
 select 
   NV.ma_nhan_vien, 
   NV.ho_ten 
-from nhan_vien NV where NV.ho_ten regexp '^[H|T|K].{0,14}$';
-  
-select 
-  NV.ma_nhan_vien, 
-  NV.ho_ten 
-from nhan_vien NV where NV.ho_ten regexp '^[H|T|K]' and char_length(NV.ho_ten) <= 15;
+from nhan_vien NV where left(substring_index(NV.ho_ten,' ', -1),1) in ('H', 'T', 'K');
+-- cách kiểm tra số ký tự đối với tên có dấu
+
   
 -- 3.  Hiển thị thông tin của tất cả khách hàng có độ tuổi từ 18 đến 50 tuổi và có địa chỉ ở “Đà Nẵng” hoặc “Quảng Trị”.
 select 
@@ -47,3 +44,21 @@ order by
 -- tong_tien (Với tổng tiền được tính theo công thức như sau: Chi Phí Thuê + Số Lượng * Giá, với Số Lượng 
 -- và Giá là từ bảng dich_vu_di_kem, hop_dong_chi_tiet) cho tất cả các khách hàng đã từng đặt phòng. 
 -- (những khách hàng nào chưa từng đặt phòng cũng phải hiển thị ra).
+select 
+   kh.ma_khach_hang,
+    kh.ho_ten, 
+    lk.ten_loai_khach, 
+    hd.ma_hop_dong, 
+    dv.ten_dich_vu, 
+    hd.ngay_lam_hop_dong, 
+    hd.ngay_ket_thuc,
+    ifnull(dv.chi_phi_thue, 0) + sum((ifnull(hdct.so_luong,0) * ifnull(dvdk.gia,0))) as tong_tien
+   from khach_hang kh
+left join hop_dong hd on kh.ma_khach_hang = hd.ma_khach_hang
+left join dich_vu dv on hd.ma_dich_vu = dv.ma_dich_vu
+left join hop_dong_chi_tiet hdct on hd.ma_hop_dong = hdct.ma_hop_dong
+left join dich_vu_di_kem dvdk on hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
+join loai_khach lk on kh.ma_loai_khach = lk.ma_loai_khach
+group by hd.ma_hop_dong;
+
+SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
