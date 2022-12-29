d7f6a9a10d9da80372b3ce88bef97b8885e5db3c
quynhngo2909 <dieuquynh105@gmail.com>
2022-12-29 23:08:28 +0700

    nop bt

---
+++ case_study/3.2.JSP_Servlet/Furama/src/main/java/com/example/furama/Furama.java
@@ -0,0 +1,21 @@
+package com.example.furama;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "furama", value = "/furama")
+public class Furama extends HttpServlet {
+
+    public void init() {
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    }
+
+    public void destroy() {
+    }
+}
